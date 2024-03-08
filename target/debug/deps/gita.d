d5ebd6f3c20917004b5bbdf01024e567bbc54820
EseToni <falsigfx@gmail.com>
2024-03-08 15:41:29 +0100

    v1.0.0

---
+++ target/debug/deps/gita.d
@@ -0,0 +1,10 @@
+C:\Users\salta\Documents\rust\gita-build\target\debug\deps\gita.exe: src\main.rs src\diff.rs src\gita_auto.rs src\gita_base.rs src\gita_restore.rs src\utils.rs
+
+C:\Users\salta\Documents\rust\gita-build\target\debug\deps\gita.d: src\main.rs src\diff.rs src\gita_auto.rs src\gita_base.rs src\gita_restore.rs src\utils.rs
+
+src\main.rs:
+src\diff.rs:
+src\gita_auto.rs:
+src\gita_base.rs:
+src\gita_restore.rs:
+src\utils.rs:
